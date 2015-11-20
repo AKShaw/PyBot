5d0843bc72e23c578d63a62d6db6b1932df2cac5
Loz <lmarshall@salisbury6c.ac.uk>
2015-11-20 13:21:32 +0000

    updated USsensor file to contain instructions for obtaining distance

--- ussensor.py
+++ ussensor.py
@@ -1,8 +1,10 @@
 from pins import Pins
+from time import *
+import RPi GPIO as GPIO
 
 class UltraSonic(Pins):
 	def __init__(self, trig, echo):
-		super().__init__(pin1, pin2)
+		super().__init__(trig, echo)
 		self.__trig = trig
 		self.__echo = echo
 
@@ -11,15 +13,30 @@ class UltraSonic(Pins):
 	    return self.__trig
 
     @trig.setter
-    def trig(self):
+    def trig(self, value):
     	self.__trig = value
 
     @property
     def echo(self):
-        return self._echo
+        return self.__echo
 
     @echo.setter
-    def echo(self):
+    def echo(self, value):
     	self.__echo = value
 
 	def US(self, trig, echo):
+        GPIO.output(trig, True)
+		time.sleep(0.00001)
+		GPIO.output(trig, False)
+
+		while GPIO.input(echo)==0:
+		  pulse_start = time.time()
+
+		while GPIO.input(echo)==1:
+		  pulse_end = time.time()
+
+		pulse_duration = pulse_end - pulse_start
+
+		distance = pulse_duration * 17150
+
+		return round(distance, 2)
