e26b22581b4a31d92b69f139198fd89573e9fe5f
Loz <lmarshall@salisbury6c.ac.uk>
2015-11-23 11:18:33 +0000

    pins updated to new layout from newsetup

--- pins.py
+++ pins.py
@@ -1,58 +1,11 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 
-
-class Pins:
-    def __init__(self, leftMF, leftMB, rightMF, rightMB, leftUSTrig, leftUSEcho, rightUSTrig, rightUSEcho):
-        self.__leftMF = leftMF
-        self.__leftMB = leftMB
-        self.__rightMF = rightMF
-        self.__rightMB = rightMB
-        self.__leftUSTrig = leftUSTrig
-        self.__leftUSEcho = leftUSEcho
-        self.__rightUSTrig = rightUSTrig
-        self.__rightUSEcho = rightUSEcho
-
-    @property
-    def LeftMF(self):
-        return self.__leftMF
-
-    @property
-    def LeftMB(self):
-        return self.__leftMB
-
-    @property
-    def RightMF(self):
-        return self.__rightMF
-
-    @property
-    def RightMB(self):
-        return self.__rightMB
-
-    @property
-    def LeftUSTrig(self):
-        return self.__leftUSTrig
-
-    @property
-    def LeftUSEcho(self):
-        return self.__leftUSEcho
-
-    @property
-    def RightUSTrig(self):
-        return self.__rightUSTrig
-
-    @property
-    def RightUSEcho(self):
-        return self.__rightUSEcho
-
-    def setup(self, LeftUSTrig, RightUSTrig, LeftUSEcho, RightUSEcho):
-        # In: Trig
-        # Out: Echo, motors
-        inPins = [LeftUSTrig, RightUSTrig]
-        print(inPins)
-        outPins = [LeftUSEcho, RightUSEcho]
-        print (outPins)
-        GPIO.setmode(GPIO.BOARD)
-        for i in inPins:
-            GPIO.setup(i, GPIO.IN)
-        for j in outPins:
-            GPIO.setup(j, GPIO.OUT)
+class Pins(object):
+    def __init__(self, IO_pins, type):
+        self.IO_pins = IO_pins
+        if (type=="sensor"):
+            gpio.setup(self.IO_pins[0], gpio.IN)  # io_pins[0] referring to the first value of the tuple e.g. pin 21 (trig)
+            gpio.setup(self.IO_pins[1], gpio.OUT)  # io_pins[1] reffering to the next value we entered e.g. pin 22 (echo)
+        elif (type=="motor"):
+            gpio.setup(self.IO_pins[0], gpio.OUT)
+            gpio.setup(self.IO_pins[1], gpio.OUT)
