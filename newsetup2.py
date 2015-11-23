422598873203d609235562cf2e053ef641575a7b
Loz <lmarshall@salisbury6c.ac.uk>
2015-11-23 11:32:18 +0000

    updated pins x3 lel

--- newsetup2.py
+++ newsetup2.py
@@ -18,14 +18,14 @@ class UltraSonic(Pins):
         super(UltraSonic, self).__init__(pins)
 
     def sensor_detect(self):
-        gpio.output(self.IO_pins[0], True)
+        gpio.output(self.IO_pins[1], True)
         time.sleep(0.00001)
-        gpio.output(self.IO_pins[0], False)
+        gpio.output(self.IO_pins[1], False)
 
-        while gpio.input(self.IO_pins[1]) == 0:
+        while gpio.input(self.IO_pins[0]) == 0:
             pulse_start = time.time()
 
-        while gpio.input(self.IO_pins[1]) == 1:
+        while gpio.input(self.IO_pins[0]) == 1:
             pulse_end = time.time()
 
         pulse_duration = pulse_end - pulse_start
