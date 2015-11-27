759099b678bd8fe77d993728cd00427861d5ef37
Loz <lmarshall@salisbury6c.ac.uk>
2015-11-27 14:46:03 +0000

    Starting building algorithms and entering in first steps the robot will take to calculate it's position

--- algorithms.py
+++ algorithms.py
@@ -5,35 +5,30 @@ class check_sensors():
         #sensor_distance[1] = left sensor distance
         #sensor_distance[2] = center sensor distance
 
+    """def check_start(self):
+        if (self.sensor_distance[2] >= 0.15):
+            if (self.sensor_distance[0,1] < 0.15) or (self.sensor_distance[0,1] > 0.45):        #trying to achieve checking both sensors for a case where they are outside of the ranges.
+                                                                                                #dont know if this works or not though
+                return "CALC"   #calcalate position method. using feedback from sensors
+            elif (0.15 <= self.sensor_distance[0,1] <= 0.45):
+                return "GOOD"   #nothing needs to change. start program
+        elif (self.sensor_distance[2] < 0.15):
+            if (self.sensor_distance[0,1] < 0.15) or (self.sensor_distance[0,1] > 0.45):
+                return "REV_CALC"
+            elif (0.15 <= self.sensor_distance[0,1] <= 0.45):
+                return "REV"
+    """
 
-    """def check_front(self):
-        if (self.sensor_distance[0] or self.sensor_distance[1]) < 0.15:
-            return "BAD"     #not sure of BAD45, as i may implement it to do the calibration for all instances of 'BAD'
-            #return "BAD45"  #BAD45 will indicate it needs a 45 degree rotation in the opposite direction based which sensor the reading came from
-        else:
-            return "GOOD"   #this will refer back to the main and indicate that no change in position needs to be made just yet"""
-
-    """def check_front(self):
+    @property
+    def check_start(self):
         if self.sensor_distance[2] < 0.15:
-            #is the front sensor below 15cm?
-            return "BAD_REV"
+            if (self.sensor_distance[0,1] < 0.15) or (self.sensor_distance[0,1] > 0.45):
+                return "REV_CALC"
+            elif (0.15 <= self.sensor_distance[0,1] <= 0.45):
+                return "REV"
         else:
-            return "GOOD"
-
-    def check_start(self):
-        if 0.15 < (self.sensor_distance[0] or self.sensor_distance[1]) < 0.45:
-            self.check_front()
-        else:
-            self.check_front"""
-
-    def check_start(self):
-        if (self.sensor_distance[2] >= 0.15) and ((self.sensor_distance[0] or self.sensor_distance[1]) < 0.15):
-            return "CALC"    #calcalate position method. using feedback from sensors
-        elif (self.sensor_distance[2] < 0.15) and ((self.sensor_distance[0] or self.sensor_distance[1]) < 0.15):
-            return "REV_CALC"    #reverse method, following calculate position method
-        elif (self.sensor_distance[2] < 0.15) and (0.15 < (self.sensor_distance[0] or self.sensor_distance[1]) < 0.45):
-            return "REV_CALC"    #reverse method only
-        elif (self.sensor_distance[2] >= 0.15) and (0.15 < (self.sensor_distance[0] or self.sensor_distance[1]) < 0.45):
-            return "GOOD"   #nothing needs to change, start program.
-        else:
-            return "CALC"   #calculate position method to get back on track
+            if (self.sensor_distance[0,1] < 0.15) or (self.sensor_distance[0,1] > 0.45):        #trying to achieve checking both sensors for a case where they are outside of the ranges.
+                                                                                                #dont know if this works or not though
+                return "CALC"   #calcalate position method. using feedback from sensors
+            elif 0.15 <= self.sensor_distance[0, 1] <= 0.45:
+                return "GOOD"   #nothing needs to change. start program
