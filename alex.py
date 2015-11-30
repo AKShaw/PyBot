0df5fc5e5dbf5fd59613580b5f88d29445bcc519
Alex Shaw <akshaw1297@gmail.com>
2015-11-30 11:29:31 +0000

    some stuff

---
+++ alex.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as gpio
+from ussensor import UltraSonic
+from algorithms import *
+from motor import Motor
+import time
+
+
+rightMotor = Motor((24,26))
+print (rightMotor)
+leftMotor = Motor((21,19))
+print (leftMotor)
+
+while True:
+    rightMotor.moveForward(100)
+    leftMotor.moveForward(100)
