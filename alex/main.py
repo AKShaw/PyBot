24ba10c8893ecfb80d9fa7835b5ce58c1859f16b
Alex Shaw <akshaw1297@gmail.com>
2015-12-07 12:31:05 +0000

    Changed speeds

--- alex/main.py
+++ alex/main.py
@@ -8,13 +8,13 @@ gpio.setmode(gpio.BOARD)
 motors = Motor()
 
 motors.moveForward(20)
-time.sleep(1)
+time.sleep(5)
 motors.moveBackward(20)
-time.sleep(1)
+time.sleep(5)
 motors.spinForward(10,20)
-time.sleep(0.5)
+time.sleep(2.5)
 motors.spinForward(20,10)
-time.sleep(0.5)
+time.sleep(2.5)
 
 gpio.cleanup()
 
