163d9bd5058cc04aae2d0afba8a3687a18ce5c22
Alex Shaw <akshaw1297@gmail.com>
2015-12-13 21:44:52 +0000

    Motor test for /alex/

---
+++ alex/motortest.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as gpio
+from motor import Motor
+import threading
+import time
+
+gpio.setmode(gpio.BOARD)
+
+motors = Motor((26,24,19,21), "motor")
+
+motors.moveForward(100, 3)
+motors.moveBackward(100, 3)
+motors.spinForward(0, 100, 3)
+motors.spinForward(100, 0, 3)
+motors.pivot("left", 50, 1)
+motors.pivot("right", 50, 1)
+
+
+gpio.cleanup()
+
+
+
