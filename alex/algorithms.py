ad0c1ffa32c4184aaa26e400c8dfa9dffcb9562a
Alex Shaw <akshaw1297@gmail.com>
2015-12-14 12:21:17 +0000

    Piss off loz

--- alex/algorithms.py
+++ alex/algorithms.py
@@ -1,10 +1,57 @@
 import time
+from motor import Motor
+from ussensor import UltraSonic
+import math
 
 
-"""class TimeToTurn():
-    def __init__(self, distanceToWall):
-        self.distanceToWall = distanceToWall
+class Straighten:
+    def __init__(self):
+        global leftUS, rightUS, motors
+        leftUS = UltraSonic((38,40), "sensor")
+        rightUS = UltraSonic((37,35), "sensor")
+        motors = Motor((26,24,19,21), "motor")
 
+    @property
+    def motors(self):
+        return motors
 
-    def calcTime(self, distanceToWall):"""
+    def getTheta():
+        """GETS THE ROBOTS ANGLE"""
+        leftDist = leftUS.sensor_detect()
+        print("LEFT US: " + str(leftDist))
+        rightDist = rightUS.sensor_detect()
+        print("RIGHT US: " + str(rightDist))
+        #totalWidth (hypotenuse) = leftUS + rightUS + robotWidth
+        totalWidth = leftDist + rightDist + 6
+        try:
+            print (math.acos(100/totalWidth))
+            return math.acos(100/totalWidth)
+        except ValueError:
+            return 0
+
+    def straighten(self):
+        initTheta = self.getTheta()
+        time.sleep(1)
+        newTheta = self.getTheta()
+        #Checks if the robot is pointed even further of course or not, corrects for whichever
+        if newTheta < initTheta:
+            while self.getTheta() >= 0.122:   #Spins while the robot is pointed more than 0.122 rads from straight
+                motors.pivot("left",20, 1) #spin left for 1 second
+        elif newTheta > initTheta:
+            while self.getTheta() >= 0.122:
+                motors.pivot("right", 20, 1) #spin right for 1 second
+
+    def start(self):
+        """ANGLES THE ROBOT MORE OR LESS STRAIGHT"""
+        initTheta = self.getTheta()
+        motors.pivot("left", 20, 1) #spin left for 1 second
+        newTheta = self.getTheta()
+        #Checks if the robot is pointed even further of course or not, corrects for whichever
+        if newTheta < initTheta:
+            while self.getTheta() >=0.122:   #Spins while the robot is pointed more than 0.122 rads from straight
+                motors.pivot("left",20, 1) #spin left for 1 second
+        elif newTheta > initTheta:
+            while self.getTheta() >= 0.122:
+                motors.pivot("right", 20, 1) #spin right for 1 second
+
 
--- alex/main.py
+++ alex/main.py
@@ -1,47 +1,15 @@
 import RPi.GPIO as gpio
 from motor import Motor
-from ussensor import UltraSonic
-import math
-from  algorithms import *
+from algorithms import Straighten
 
 
-def setup():
-    """SETS UP BOARD, SENSORS AND MOTORS"""
-    gpio.setmode(gpio.BOARD)
-    global leftUS, rightUS, motors
-    leftUS = UltraSonic((38,40), "sensor")
-    rightUS = UltraSonic((37,35), "sensor")
-    motors = Motor((26,24,19,21), "motor")
+gpio.setmode(gpio.BOARD)
 
-def start():
-    """ANGLES THE ROBOT MORE OR LESS STRAIGHT"""
-    initTheta = getTheta()
-    motors.pivot("left", 20, 1) #spin left for 1 second
-    newTheta = getTheta()
-    #Checks if the robot is pointed even further of course or not, corrects for whichever
-    if newTheta < initTheta:
-        while getTheta() >=0.122:   #Spins while the robot is pointed more than 0.122 rads from straight
-            motors.pivot("left",20, 1) #spin left for 1 second
-    elif newTheta > initTheta:
-        while getTheta() >= 0.122:
-            motors.pivot("right", 20, 1) #spin right for 1 second
+a = Straighten()
+a.start()
 
-def getTheta():
-    """GETS THE ROBOTS ANGLE"""
-    leftDist = leftUS.sensor_detect()
-    print("LEFT US: " + str(leftDist))
-    rightDist = rightUS.sensor_detect()
-    print("RIGHT US: " + str(rightDist))
-    #totalWidth (hypotenuse) = leftUS + rightUS + robotWidth
-    totalWidth = leftDist + rightDist + 6
-    try:
-        print (math.acos(100/totalWidth))
-        return math.acos(100/totalWidth)
-    except ValueError:
-        return 0
+while True:
+    a.motors.moveForward(100, 2.5)
+    a.straighten()
 
-
-setup()
-start()
-motors.moveForward(50, 4)
 gpio.cleanup()
