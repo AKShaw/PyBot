930c54a7bfcf19883d7b5b144b97bc90bad7e908
Alex Shaw <akshaw1297@gmail.com>
2015-12-15 11:41:31 +0000

    Added io definition, changed the straighten object to use this

--- alex/main.py
+++ alex/main.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as gpio
 from algorithms import Straighten
+from io import InputOutput
 
 
 gpio.setmode(gpio.BOARD)
+io = InputOutput()
 
-a = Straighten()
+a = Straighten(io)
 a.start()
 
 """while True:
-    a.motors.moveForward(100, 2.5)
+    io.motors.moveForward(100, 2.5)
     a.straighten()"""
 
 gpio.cleanup()
