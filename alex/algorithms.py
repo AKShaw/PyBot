2788af8eeecab58fc4fc73106579009b2cc81574
Alex Shaw <akshaw1297@gmail.com>
2015-12-16 11:54:28 +0000

    Removed IO class

--- alex/algorithms.py
+++ alex/algorithms.py
@@ -1,26 +1,33 @@
 import math
 import time
-
+from motor import Motor
+from ussensor import UltraSonic
 
 class Straighten:
-    def __init__(self, io):
-        self.io = io
+    def __init__(self):
+        self.motors = Motor((26, 24, 19, 21), "motor")
+        self.leftUS = UltraSonic((38, 40), "sensor")
+        self.rightUS = UltraSonic((37, 35), "sensor")
+
+    @property
+    def motors(self):
+        return self.motors
 
     def getTrackWidth(self):
-        self.io.motors.pivot("left", 50, 5)
+        self.motors.pivot("left", 50, 5)
         width_list = []
         for i in range(0, 20):
-            right_distance = self.io.rightUS.sensor_detect()
-            left_distance = self.io.leftUS.sensor_detect()
+            right_distance = self.rightUS.sensor_detect()
+            left_distance = self.leftUS.sensor_detect()
             width_list.insert(i, right_distance+left_distance+6)
             time.sleep(0.25)
         return min(width_list)
 
     def getTheta(self, trackWidth):
         """GETS THE ROBOTS ANGLE"""
-        leftDist = self.io.leftUS.sensor_detect()
+        leftDist = self.leftUS.sensor_detect()
         print("LEFT US: " + str(leftDist))
-        rightDist = self.io.rightUS.sensor_detect()
+        rightDist = self.rightUS.sensor_detect()
         print("RIGHT US: " + str(rightDist))
         #totalWidth (hypotenuse) = leftUS + rightUS + robotWidth
         totalWidth = leftDist + rightDist + 6
@@ -38,14 +45,14 @@ class Straighten:
         trackWidth = self.getTrackWidth()
         print(str(trackWidth))
         initTheta = self.getTheta(trackWidth)
-        self.io.motors.pivot("left", 20, 0.5) #spin left for 1 second
+        self.motors.pivot("left", 20, 0.5) #spin left for 1 second
         print("Moved")
         newTheta = self.getTheta(trackWidth)
         #Checks if the robot is pointed even further of course or not, corrects for whichever
         if newTheta < initTheta:
             while self.getTheta(trackWidth) >=0.122:   #Spins while the robot is pointed more than 0.122 rads from straight
-                self.io.motors.pivot("left", 20, 1) #spin left for 0.25 second
+                self.motors.pivot("left", 20, 1) #spin left for 0.25 second
         elif newTheta > initTheta:
             while self.getTheta(trackWidth) >= 0.122:
-                self.io.motors.pivot("right", 20, 1) #spin right for 0.25 second
+                self.motors.pivot("right", 20, 1) #spin right for 0.25 second
 
--- alex/main.py
+++ alex/main.py
@@ -1,23 +1,15 @@
 import RPi.GPIO as gpio
-from motor import Motor
-from ussensor import UltraSonic
+
 from algorithms import Straighten
-from ioclass import InputOutput
 
 
 gpio.setmode(gpio.BOARD)
 
-motors = Motor((26, 24, 19, 21), "motor")
-leftUS = UltraSonic((38, 40), "sensor")
-rightUS = UltraSonic((37, 35), "sensor")
-
-io = InputOutput(motors, leftUS, rightUS)
-
-a = Straighten(io)
+a = Straighten()
 a.start()
 
 """while True:
-    io.motors.moveForward(100, 2.5)
+    a.motors.moveForward(100, 2.5)
     a.straighten()"""
 
 gpio.cleanup()
