b69aa801961507db24a6ea189f03b4bce1cb55ac
Loz <lmarshall@salisbury6c.ac.uk>
2016-01-06 12:44:28 +0000

    tidied up code

--- alex/pins.py
+++ alex/pins.py
@@ -7,11 +7,13 @@ class Pins(object):
         if (type=="sensor"):
             gpio.setup(self.IO_pins[0], gpio.IN)    #io_pins[0] referring to the first value in the tuple (echo)
             gpio.setup(self.IO_pins[1], gpio.OUT)   #io_pins[1] reffering to the next value in the type (trig)
+            print("US sensor pins set")
         elif (type=="motor"):
             gpio.setup(self.IO_pins[0], gpio.OUT)   #io_pins[0] will be motor forward
             gpio.setup(self.IO_pins[1], gpio.OUT)   #io_pins[1] will be motor backward
             gpio.setup(self.IO_pins[2], gpio.OUT)   #io_pins[2] will be motor forward
             gpio.setup(self.IO_pins[3], gpio.OUT)   #io_pins[3] will be motor backward#
-            print("pins set correctly")
+            print("motor pins set")
         elif (type=="infrared"):
             gpio.setup(self.IO_pins[0], gpio.OUT)   #io_pins[0] is the only one required by infrared sensors.
+            print("Infrared senesor pins set")
