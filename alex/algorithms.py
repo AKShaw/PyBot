8c0f3f8a2b0be9fa8d0f682e9e1bccfb890b4237
LozMarshall <lmarshall@salisbury6c.ac.uk>
2016-02-08 11:35:58 +0000

    updated print statements and tidied code

--- alex/algorithms.py
+++ alex/algorithms.py
@@ -24,7 +24,7 @@ class Straighten:
             right_distance = rightUS.sensor_detect()
             left_distance = leftUS.sensor_detect()
             width_list.insert(i, right_distance+left_distance+6)
-            time.sleep(0.08)
+            time.sleep(0.064)
             print("input " + str(i) + " received")
         print("FINISHED GETTING TRACK WIDTH")
         return min(width_list)
